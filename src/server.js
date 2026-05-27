f24f906533fb540a22d0aa81b89dcaf43ae2f918
poomichopoom-p <Poomicho.p@gmail.com>
2026-05-27 15:44:13 +0700

    edit-url

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 const corsOptions = {
   origin: [
-    "https://full-stackapp-frontend.vercel.app/",
+    "https://full-stackapp-frontend.vercel.app",
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:5175",
