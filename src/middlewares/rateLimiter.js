76a9fa5acdb11698fb3a703d9928eea0b4499e01
poomichopoom-p <Poomicho.p@gmail.com>
2026-05-27 14:35:54 +0700

    debug

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -1,9 +1,9 @@
 import  rateLimit  from "express-rate-limit";
 
  export const limiter = rateLimit({
-  window: 15 * 60 * 1000,
+  windowMs: 15 * 60 * 1000,
   max: 100,
-  standarHeaders: true,
+  standardHeaders: true,
   legacyHeaders: false,
 });
 
